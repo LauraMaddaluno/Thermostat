6ff644359919cb9c077dad6a4a67a621ad254304
LauraMaddaluno <lauralondon.4@gmail.com>
2021-09-28 19:17:11 +0100

    refactoring test using before

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,58 +1,55 @@
 describe("Thermostat", function() {
   'use strict'
 
+  let thermostat;
+
+  beforeEach(() => {
+   thermostat = new Thermostat();
+  });
+
   it("should have a start temperature of 20 degrees", function() {
-    let thermostat = new Thermostat();
     expect(thermostat.getTemperature()).toEqual(20);
    })
 
    it("should be possible to increase the temperature", function() {
-    let thermostat = new Thermostat();
     thermostat.increaseTemp(5)
     expect(thermostat.getTemperature()).toEqual(25);
    })
 
    it("should be possible to decrease the temperature", function() {
-    let thermostat = new Thermostat();
     thermostat.decreaseTemp(5)
     expect(thermostat.getTemperature()).toEqual(15);
    })
 
    it("should have a minimum temperature of 10 degrees", function() {
-    let thermostat = new Thermostat();
     expect(thermostat.minimumTemp).toEqual(10);
    })
 
    it("should have a maximum temperature of 25 degrees when power save is on", function() {
-    let thermostat = new Thermostat();
     expect(thermostat.MAX_TEMP_PSM_ON ).toEqual(25);
    })
 
    it("should have a maximum temperature of 32 degrees when power save is off", function() {
-    let thermostat = new Thermostat();
     thermostat.powerSaveMode(false)
     expect(thermostat.MAX_TEMP_PSM_OFF).toEqual(32);
    })
+
    it("should reset the temperature to 20 degrees", function() {
-    let thermostat = new Thermostat();
     thermostat.increaseTemp(5)
     thermostat.reset()
     expect(thermostat.temperature).toEqual(20);
    })
 
    it("should show the energy usage as medium-usage when <=25", function() {
-    let thermostat = new Thermostat();
     expect(thermostat.energyUsage()).toEqual("medium-usage");
    })
 
    it("should show the energy usage as low-usage when <18", function() {
-    let thermostat = new Thermostat();
     thermostat.decreaseTemp(5)
     expect(thermostat.energyUsage()).toEqual("low-usage");
    })
 
-   it("should show the energy usage as high-usage otherwise", function() {
-    let thermostat = new Thermostat();
+   it("should show the energy usage as high-usage otherwise", function(){ 
     thermostat.increaseTemp(10)
     expect(thermostat.energyUsage()).toEqual("high-usage");
    })
